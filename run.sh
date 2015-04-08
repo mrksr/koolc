cede46a748aaf1b958e6111cbeced9b0fdf9b62c
Markus Kaiser <markus.kaiser@in.tum.de>
2015-04-08 12:59:23 +0200

    debugging script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+REFC="scala -cp ./refc/koolc_2.11-1.2.jar:./refc/cafebabe_2.11-1.2.jar koolc.Main"
+MYC="scala -cp ./target/scala-2.11/koolc_2.11-1.0.jar koolc.Main"
+TESTDIR="./testprograms/lab3/valid"
+
+for p in $TESTDIR/*; do
+    echo "*************************"
+    echo "* Testing: $p"
+    $REFC --tokens $p > /tmp/reftokens.koolc
+    $MYC  --tokens $p > /tmp/mytokens.koolc
+    diff -q /tmp/reftokens.koolc /tmp/mytokens.koolc
+done
+
+for p in $TESTDIR/*; do
+    echo "*************************"
+    echo "* Testing AST: $p"
+    $REFC --ast $p               > /tmp/refast.koolc
+    $MYC  --ast $p               > /tmp/myast.koolc
+    $REFC --ast /tmp/myast.koolc > /tmp/normast.koolc
+    diff -q /tmp/refast.koolc /tmp/normast.koolc
+done
+
