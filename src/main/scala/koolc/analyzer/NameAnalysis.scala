606f25e4baaf7ff5d5c1f55bcaa39802cd7d1492
Markus Kaiser <markus.kaiser@in.tum.de>
2015-04-08 23:33:38 +0200

    add warning for unused classes and variables

--- src/main/scala/koolc/analyzer/NameAnalysis.scala
+++ src/main/scala/koolc/analyzer/NameAnalysis.scala
@@ -8,16 +8,17 @@ import Symbols._
 object NameAnalysis extends Pipeline[Program, Program] {
   def run(ctx: Context)(prog: Program): Program = {
     // All methods here have heavy side-effects!
-    val gs = initial(ctx, prog)
+    val (gs, vars, clss) = initial(ctx, prog)
     ctx.reporter.terminateIfErrors
-    attach(ctx, prog, gs)
+    attach(ctx, prog, gs, vars, clss)
     ctx.reporter.terminateIfErrors
     inheritance(ctx, prog, gs)
     ctx.reporter.terminateIfErrors
     prog
   }
 
-  def initial[S <: Symbol](ctx: Context, prog: Program): GlobalScope = {
+  var vars = Set[VariableSymbol]()
+  def initial[S <: Symbol](ctx: Context, prog: Program) = {
     def doubleDeclaration[T <: Symbol](a: T, b: T) {
       val tpe = a match {
         case _: ClassSymbol => "Class-"
@@ -29,7 +30,7 @@ object NameAnalysis extends Pipeline[Program, Program] {
       ctx.reporter.error("First declaration is here.", a)
     }
 
-    def map[S <: Symbol](ss: List[Symbolic[S]], taken: Map[String, S]): Map[String, S] = {
+    def map[S <: Symbol](ss: List[Symbolic[S]], taken: Map[String, S]): Map[String, S] =
       (Map[String, S]() /: ss) {
         (m, sc) => {
           val s = sc.getSymbol
@@ -41,83 +42,86 @@ object NameAnalysis extends Pipeline[Program, Program] {
           }
         }
       }
-    }
 
-    def propagate[S <: Symbol](t: Tree, parent: Option[Symbolic[S]]): Unit = {
-      t match {
-        case o@MainObject(id, stats) => {
-          val s = o.setSymbol(new ClassSymbol(id.value)).getSymbol
-          s.setPos(o)
-          id.setSymbol(s)
+    def propagate[S <: Symbol](t: Tree, parent: Option[Symbolic[S]]): Unit = t match {
+      case o@MainObject(id, stats) => {
+        val s = o.setSymbol(new ClassSymbol(id.value)).getSymbol
+        s.setPos(o)
+        id.setSymbol(s)
 
-          s.members = Map()
-          s.methods = Map()
-        }
-
-        case c@ClassDecl(id, parent, vars, methods) => {
-          val s = c.setSymbol(new ClassSymbol(id.value)).getSymbol
-          s.setPos(c)
-          id.setSymbol(s)
-
-          vars.foreach(propagate(_, Some(c)))
-          methods.foreach(propagate(_, Some(c)))
-
-          s.members = map(vars, Map())
-          s.methods = map(methods, Map())
-        }
-
-        case m@MethodDecl(retType, id, args, vars, stats, retExpr) => {
-          parent.get.getSymbol match {
-            case ps: ClassSymbol =>
-              val s = m.setSymbol(new MethodSymbol(id.value, ps)).getSymbol
-              s.setPos(m)
-              id.setSymbol(s)
-
-            case _ =>
-          }
-
-          args.foreach(propagate(_, Some(m)))
-          vars.foreach(propagate(_, Some(m)))
-
-          val s = m.getSymbol
-          s.params = map(args, Map())
-          s.members = map(vars, s.params)
-        }
-
-        case v@VarDecl(tpe, id) => {
-          val s = v.setSymbol(new VariableSymbol(id.value)).getSymbol
-          s.setPos(v)
-          id.setSymbol(s)
-        }
-
-        case f@Formal(tpe, id) => {
-          val s = f.setSymbol(new VariableSymbol(id.value)).getSymbol
-          s.setPos(f)
-          id.setSymbol(s)
-        }
-
-        case _ =>
+        s.members = Map()
+        s.methods = Map()
       }
+
+      case c@ClassDecl(id, parent, vars, methods) => {
+        val s = c.setSymbol(new ClassSymbol(id.value)).getSymbol
+        s.setPos(c)
+        id.setSymbol(s)
+
+        vars.foreach(propagate(_, Some(c)))
+        methods.foreach(propagate(_, Some(c)))
+
+        s.members = map(vars, Map())
+        s.methods = map(methods, Map())
+      }
+
+      case m@MethodDecl(retType, id, args, vars, stats, retExpr) => {
+        parent.get.getSymbol match {
+          case ps: ClassSymbol =>
+            val s = m.setSymbol(new MethodSymbol(id.value, ps)).getSymbol
+            s.setPos(m)
+            id.setSymbol(s)
+
+          case _ =>
+        }
+
+        args.foreach(propagate(_, Some(m)))
+        vars.foreach(propagate(_, Some(m)))
+
+        val s = m.getSymbol
+        s.params = map(args, Map())
+        s.members = map(vars, s.params)
+      }
+
+      case v@VarDecl(tpe, id) => {
+        val s = v.setSymbol(new VariableSymbol(id.value)).getSymbol
+        s.setPos(v)
+        id.setSymbol(s)
+        vars += s
+      }
+
+      case f@Formal(tpe, id) => {
+        val s = f.setSymbol(new VariableSymbol(id.value)).getSymbol
+        s.setPos(f)
+        id.setSymbol(s)
+        vars += s
+      }
+
+      case _ =>
     }
 
-    val s = new GlobalScope()
-
+    val gs = new GlobalScope()
     val Program(main, classes) = prog
+
     propagate(main, None)
     classes.foreach(propagate(_, None))
 
-    s.mainClass = main.getSymbol
-    s.classes = map(classes, Map())
+    gs.mainClass = main.getSymbol
+    gs.classes = map(classes, Map())
 
-    if (s.classes.contains(s.mainClass.name)) {
-      doubleDeclaration(s.mainClass, s.classes(s.mainClass.name))
-      s.classes = s.classes - s.mainClass.name
+    if (gs.classes.contains(gs.mainClass.name)) {
+      doubleDeclaration(gs.mainClass, gs.classes(gs.mainClass.name))
+      gs.classes = gs.classes - gs.mainClass.name
     }
 
-    s
+    val clss = gs.classes.values.toSet
+    (gs, vars, clss)
   }
 
-  def attach(ctx: Context, prog: Program, gs: GlobalScope): Unit = {
+  def attach(ctx: Context, prog: Program, gs: GlobalScope,
+    vars: Set[VariableSymbol], clss: Set[ClassSymbol]): Unit = {
+    var uvars = vars
+    var uclss = clss
     def propagate[S <: Symbol](t: Tree, parent: Option[Symbolic[S]]): Unit = {
       t match {
         case Program(main, classes) => {
@@ -166,6 +170,7 @@ object NameAnalysis extends Pipeline[Program, Program] {
         if (gs.classes.contains(value)) {
           val s = gs.classes(value)
           i.setSymbol(s)
+          uclss -= s
           Some(s)
         } else if (value == gs.mainClass.name) {
           ctx.reporter.error("The main class '%s' is not a valid type.".format(value), i)
@@ -211,7 +216,7 @@ object NameAnalysis extends Pipeline[Program, Program] {
       case MethodCall(obj, meth, args) => expression(ms, obj); args.foreach(expression(ms, _))
 
       case i@Identifier(value) => variable(ms, i)
-      case t@This() => t.setSymbol(ms.classSymbol)
+      case t@This() => t.setSymbol(ms.classSymbol); uclss -= ms.classSymbol
 
       case _ =>
     }
@@ -220,12 +225,19 @@ object NameAnalysis extends Pipeline[Program, Program] {
       val Identifier(value) = id
 
       ms.lookupVar(value).orElse(ms.classSymbol.lookupVar(value)) match {
-        case Some(s) => id.setSymbol(s)
+        case Some(s) => id.setSymbol(s); uvars -= s
         case None => ctx.reporter.error("Use of undeclared variable '%s'.".format(value), id)
       }
     }
 
     propagate(prog, None)
+
+    uvars.foreach {
+      v => ctx.reporter.warning("Variable '%s' defined but never used.".format(v.name), v)
+    }
+    uclss.foreach {
+      c => ctx.reporter.warning("Class '%s' defined but never used.".format(c.name), c)
+    }
   }
 
   def inheritance(ctx: Context, prog: Program, gs: GlobalScope) {
